7a06c0d1fde8e89c86cb129cc69e20a82fa4ae61
Victor Matheus <victormatheus507@gmail.com>
2024-02-01 09:37:08 -0300

    criando objetos das cores

---
+++ script.js
@@ -0,0 +1,27 @@
+const verdeCipreste = {
+  nome: 'Verde-cipreste',
+  pasta: 'imagens-verde-cipreste',
+};
+
+const azulInverno = {
+  nome: 'Azul-inverno',
+  pasta: 'imagens-azul-inverno',
+};
+
+const meiaNoite = {
+  nome: 'Meia-noite',
+  pasta: 'imagens-meia-noite',
+};
+
+const estelar = {
+  nome: 'Estelar',
+  pasta: 'imagens-estelar',
+};
+
+const rosaClaro = {
+  nome: 'Rosa-claro',
+  pasta: 'imagens-rosa-claro',
+};
+
+const opcoesCores = [verdeCipreste, azulInverno, meiaNoite, estelar, rosaClaro]
+
